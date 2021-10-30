e00f53dde5caf7deff9dff682539797d84c072b2
daeheon han 한대헌 <eogjs99@gmail.com>
2021-10-30 16:35:26 +0900

    empty sample protocol

--- packages/protocols/src/sample/protocols/sample.ts
+++ packages/protocols/src/sample/protocols/sample.ts
@@ -1,5 +1,6 @@
 import { DatabaseConnectionMode, Protocol } from "../../common";
 import yup = require('yup');
+import { EmptyModel } from "src/shared/models";
 
 interface SampleReq {
 	a: string;
@@ -19,12 +20,22 @@ interface SampleResp {
 export namespace sampleProtocol {
 	export const resource = '/api/sample';
 
+	const empty = Protocol.api({
+		__inputType: {} as EmptyModel,
+		__outputType: {} as SampleResp,
+		method: 'post',
+		resource,
+		page: '/empty',
+		schema: EmptyModel.schema,
+		db: DatabaseConnectionMode.NO_DB,
+	})
+
 	const sample = Protocol.api({
 		__inputType: {} as SampleReq,
 		__outputType: {} as SampleResp,
 		method: 'post',
 		resource,
-		page: '/',
+		page: '/sample',
 		schema: sampleSchema,
 		db: DatabaseConnectionMode.NO_DB,
 	});
